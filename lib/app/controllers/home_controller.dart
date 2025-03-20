56aec7aa2496fdf97b8ba01565450befd25582de
JpFontinele-UFF <jfontinele@id.uff.br>
2025-03-20 15:24:53 -0300

    exclusao do antigo departamento

--- lib/app/controllers/home_controller.dart
+++ lib/app/controllers/home_controller.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:inventoryplatform/app/controllers/carousel_section_controller.dart';
-import 'package:inventoryplatform/app/data/models/department_model.dart';
 import 'package:inventoryplatform/app/routes/app_routes.dart';
 
 class HomeController extends GetxController {
@@ -9,19 +8,6 @@ class HomeController extends GetxController {
 /* final UtilsService _utilsService = UtilsService();
   final OrganizationRepository organizationRepository =
       Get.find<OrganizationRepository>();*/
-
-  final List<DepartmentModel> departments = [
-    DepartmentModel(
-      id: '2',
-      title: "Laboratórios",
-      description: 'Um departamento para testes inicialmente vazia.',
-    ),
-    DepartmentModel(
-      id: '1',
-      title: "Embarcações",
-      description: 'Um departamento de exemplo com dados fictícios.',
-    ),
-  ];
 
   @override
   void onInit() {
